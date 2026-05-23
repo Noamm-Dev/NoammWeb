f0578e15a5e8b96b169f78522b315b668625144a
Noamm9 <belkunsama@gmail.com>
2026-05-24 01:13:25 +0300

    cache

--- src/main/kotlin/NoammWeb.kt
+++ src/main/kotlin/NoammWeb.kt
@@ -1,13 +1,16 @@
 import bio.BioPage
 import database.DatabasePage
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.html.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.util.concurrent.TimeUnit
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.application.createApplicationPlugin
+import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.html.respondHtml
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.header
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import java.util.concurrent.*
 
 object NoammWeb {
     private val cacheTime = TimeUnit.HOURS.toMillis(24)
@@ -17,6 +20,14 @@ object NoammWeb {
         val port = System.getenv("PORT")?.toInt() ?: 3000
 
         embeddedServer(Netty, port) {
+            install(createApplicationPlugin("CORS") {
+                onCallRespond { call ->
+                    call.response.header("Access-Control-Allow-Origin", "*")
+                    call.response.header("Access-Control-Allow-Headers", "*")
+                    call.response.header("Access-Control-Allow-Methods", "*")
+                }
+            })
+
             routing {
                 get("/") {
                     call.response.header("Cache-Control", "public, s-maxage=${cacheTime}")
@@ -24,6 +35,7 @@ object NoammWeb {
                 }
 
                 get("/database") {
+                    call.response.header("Cache-Control", "public, s-maxage=${cacheTime}")
                     call.respondHtml(HttpStatusCode.OK, DatabasePage::get)
                 }
             }
